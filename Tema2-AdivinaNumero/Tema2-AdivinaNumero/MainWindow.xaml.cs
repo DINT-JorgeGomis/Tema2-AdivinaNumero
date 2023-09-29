0b27fa7a530b7472240a8906d5bdb8fe32d3bcd8
Jorge Gomis Román <jorge.gomis@iesdoctorbalmis.com>
2023-09-29 14:56:40 +0200

    Hecho

---
+++ Tema2-AdivinaNumero/Tema2-AdivinaNumero/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tema2_AdivinaNumero
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int NumeroRandom;
+        public MainWindow()
+        {
+            NumeroRandom = new Random().Next(0, 101);
+            InitializeComponent();
+        }
+
+        private void Comprobar(object sender, RoutedEventArgs e)
+        {
+            Respuesta.Foreground = new SolidColorBrush(Colors.Red);
+            if (int.TryParse(Caja.Text, out int result))
+            {
+                if (result == NumeroRandom) {
+                    Respuesta.Content = "ES CORRECTO!!";
+                    Respuesta.Foreground = new SolidColorBrush(Colors.Green);
+                } else
+                {
+                    Respuesta.Content = "NO ES CORRECTO";
+                }
+            } else
+            {
+                Respuesta.Content = "NO ES UN NÚMERO!!";
+            }
+        }
+
+        private void Reiniciar(object sender, RoutedEventArgs e)
+        {
+            NumeroRandom = new Random().Next(0, 101);
+            Respuesta.Content = "";
+        }
+    }
+}
